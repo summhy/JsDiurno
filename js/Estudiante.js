7d643a6ffbedf8ed9c9b953c82d689b38351a71e
Susana Muñoz <su.munozh@gmail.com>
2023-02-02 12:52:44 -0300

    cuarto commit

--- js/Estudiante.js
+++ js/Estudiante.js
@@ -1,14 +1,20 @@
 function Estudiante(nombre, apellido){
     this.nombre =  nombre;
     this.apellido =  apellido;
-    this.edad;
-    this.hobbies;
-    this.nacimiento;
+    this.edad = "";
+    this.hobbies = "";
+    this.nacimiento = "";
 
-    this.nombrar = ()=>{
-        return `El nombre del estudiante es ${this.nombre} ${this.apellido}`;
-       
+    this.nombrar = ()=>{ return `${this.apellido}, ${this.nombre}`; }
+
+    this.setEdad = (edad) =>{ this.edad =  edad; }
+   
+    this.setNacimiento = (nacimiento) =>{ this.nacimiento =  nacimiento; }
+
+    this.setHobbies = (hobbies) =>{ this.hobbies =  hobbies; }
+
+    this.getEdad = function (){
+         /* si edad retorna edad, sino si nacimiento calcular edad, sino 0 */
     }
-
 
 }
