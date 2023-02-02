4792171d4fb4a5bc5b057519e716d4c27b674b64
Susana Muñoz <su.munozh@gmail.com>
2023-02-02 12:53:00 -0300

    cuarto commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-//console.log(curso);
+const tbody = document.querySelector("#tb-estudiantes");
+
 curso.estudiantes.forEach(element => {
-    //console.log(element);
+    
     const estudiante = new Estudiante(element.nombre, element.apellido);
-
-    console.log(estudiante.nombrar());
-
+    if(element.edad){ estudiante.setEdad(element.edad); }
+    if(element.nacimiento){ estudiante.setNacimiento(element.nacimiento); }
+    if(element.hobbies){ estudiante.setHobbies(element.hobbies); }
+    tbody.innerHTML += `<tr>
+                            <td>${estudiante.nombre}</td>
+                            <td>${estudiante.apellido}</td>
+                            <td>${estudiante.nombrar()}</td>
+                            <td>${estudiante.edad}</td>
+                            <td>${estudiante.nacimiento}</td>
+                            <td>${estudiante.hobbies}</td>
+                       </tr> `;
 }); 
